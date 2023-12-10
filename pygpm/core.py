3ebb26f5c3d365ce089c04a7f2b87d29df6a4ada
BrandonPacewic <pacewicbrandon@gmail.com>
2023-12-10 11:45:13 -0800

    Version bump

--- pygpm/core.py
+++ pygpm/core.py
@@ -8,7 +8,7 @@ Core constants used throughout pygpm.
 import os
 import platform
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 HOME = os.getenv("HOME", os.getenv("USERPROFILE"))
 XDG_CACHE_DIR = os.getenv(
